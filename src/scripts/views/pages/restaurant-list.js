82917e24355760d4aacec02ec20e84d371086f1f
Em Izrofi <dev.zibril101@gmail.com>
2024-05-15 08:48:10 +0700

    update hero text header

--- src/scripts/views/pages/restaurant-list.js
+++ src/scripts/views/pages/restaurant-list.js
@@ -12,10 +12,12 @@ const RestaurantList = {
             <source media="(min-width: 320px)" data-srcset="./images/heros/hero-image_1-small.webp" class="hero__bg lazyload">
             <img data-src="./images/heros/hero-image_1.jpg" class="hero__bg lazyload" alt="Hero Background">
           </picture>
-          <div class="hero__content">
-            <h1>Welcome to Hungrify Apps</h1>
-            <p>Hungrify is a site that displays a collection of catalogs of famous restaurants in Indonesia</p>
-            <button class="btn--text btn--scroll-to">Explore Now</button>
+          <div class="hero__sec">
+            <div class="hero__content">
+              <h1>Welcome to Hungry App</h1>
+              <p>This is my final project submission from the Dicoding Indonesia class "Menjadi Front-End Web Developer Expert" using JavaScript native. This project submission covers web vitals, unit testing, best practice and more.</p>
+              <button class="btn--text btn--scroll-to">Explore Now</button>
+            </div>
           </div>
       </div>
       <div class="content m_content">
